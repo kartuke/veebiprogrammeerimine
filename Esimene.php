0fa84147845c6f1a2126007d9bf0b40a2a90e473
Kärt Nigols <kartuke@users.noreply.github.com>
2017-09-11 13:49:22 +0300

    esimene PHP fail, kus on vaid HTML

---
+++ Esimene.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <font color=white>
+<title>
+  Kärt Nigols Veebiprogrameerimise asjad
+</title>
+
+ <link rel="icon" href="https://m.popkey.co/987552/NGLb3.gif" type="GIF">
+
+</head>
+<body>
+  <h1>Kärt Nigols</h1>
+
+  <big><p><i><b>See veebileht on loodud Õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p></i></b></big>
+  <body bgcolor="#0B3B39">
+
+<img src="https://m.popkey.co/987552/NGLb3.gif" alt="Banana" style="width:450px;height:450px;">
+
+<img src="https://m.popkey.co/08e691/pJedy.gif" alt="Stan" style="width:450px;height:450px;">
+
+<img src="https://media.giphy.com/media/CY91MScPevNfi/source.gif" alt="Stan" style="width:650px;height:450px;">
+
+</body>
+</html>
